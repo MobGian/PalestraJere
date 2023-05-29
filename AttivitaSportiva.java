fb7383ac6a7a784cdc98c3a127daec0bd7bd9d53
MobGian <97670196+MobGian@users.noreply.github.com>
2023-05-29 19:32:21 +0200

    Altre classi

---
+++ AttivitaSportiva.java
@@ -0,0 +1,13 @@
+package src;
+
+public class AttivitaSportiva {
+	private String attivitaId;
+	
+	public String getAttivitaId(){
+		return attivitaId;
+	}
+	
+	public void setAttivitaId(String attivitaId) {
+		this.attivitaId = attivitaId;
+	}
+}
---
+++ Corso.java
@@ -0,0 +1,18 @@
+package src;
+
+public class Corso {
+	public String corsoId;
+	Trainer trainerId;
+	
+	public String getCorsoId() {
+		return corsoId;
+	}
+	
+	public void setCorsoId(String corsoId) {
+		this.corsoId = corsoId;
+	}
+	
+	public Trainer getTrainer() {
+		return trainerId;
+	}
+}
